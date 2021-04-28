7d8f0fce9fbbe14970ef761743604887dd565104
Toan Phung <toan.phungkhoiquoctoan@gmail.com>
2021-04-28 10:29:03 +1000

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 work = "work.txt"
 R_HOME = r"C:\Program Files\R\R-3.6.3"
 Reformat_MSstats = "reformatMS_windows_amd64.exe"
-split = True
+split = False
 msstats_cutoff = 0.00001
 gostats_cutoff = 0.01
 gostats_check = True
